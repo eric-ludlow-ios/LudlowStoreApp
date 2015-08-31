a0f4283d8b7ed90d3e2eb06a085ac94b2003c727
Eric Ludlow <esludlow@gmail.com>
2015-08-31 14:53:10 -0600

    adds inAppPurch to remove ads

---
+++ LudlowStoreApp/StoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  StoreViewController.h
+//  LudlowStoreApp
+//
+//  Created by Rutan on 8/31/15.
+//  Copyright (c) 2015 EricLudlowRadicalApplications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StoreViewController : UIViewController
+
+@end
