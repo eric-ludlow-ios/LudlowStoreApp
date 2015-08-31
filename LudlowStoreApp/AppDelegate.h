cf60e8b2410f7e9607af75df9888b5bb3fe4e732
Eric Ludlow <esludlow@gmail.com>
2015-08-31 09:20:50 -0600

    initial commit

---
+++ LudlowStoreApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LudlowStoreApp
+//
+//  Created by Rutan on 8/31/15.
+//  Copyright (c) 2015 EricLudlowRadicalApplications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LudlowStoreApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LudlowStoreApp
+//
+//  Created by Rutan on 8/31/15.
+//  Copyright (c) 2015 EricLudlowRadicalApplications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
